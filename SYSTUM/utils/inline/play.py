e60085f03aa55d69a6b1f0ed0bcbe476f5f5b142
Nobi-123 <nobita.infringement.report@gmail.com>
2025-08-17 00:50:04 +0530

    Update play.py

--- SYSTUM/utils/inline/play.py
+++ SYSTUM/utils/inline/play.py
@@ -69,7 +69,7 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
       [
             InlineKeyboardButton(
-                text="ᴏᴡɴᴇʀ", url="https://t.me/Og_Tenny",
+                text="ᴏᴡɴᴇʀ", url="https://t.me/Og_Zerathos",
             ),
             InlineKeyboardButton(
                 text="sᴜᴩᴩᴏʀᴛ", url="https://t.me/JarvisXsupport",
@@ -91,7 +91,7 @@ def stream_markup(_, chat_id):
          ],
         [
             InlineKeyboardButton(
-                text="ᴏᴡɴᴇʀ", url="https://t.me/Og_Tenny",
+                text="ᴏᴡɴᴇʀ", url="https://t.me/Og_Zerathos",
             ),
             InlineKeyboardButton(
                 text="sᴜᴩᴩᴏʀᴛ", url="https://t.me/JarvisXsupport",
